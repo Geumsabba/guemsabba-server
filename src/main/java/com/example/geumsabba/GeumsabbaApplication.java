98832dc53293b360b42cd751bb4cd9749ae12c2c
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-07-27 00:13:14 +0900

    Change java ver

---
+++ src/main/java/com/example/geumsabba/GeumsabbaApplication.java
@@ -0,0 +1,13 @@
+package com.example.geumsabba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeumsabbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeumsabbaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/geumsabba/GeumsabbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geumsabba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeumsabbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
