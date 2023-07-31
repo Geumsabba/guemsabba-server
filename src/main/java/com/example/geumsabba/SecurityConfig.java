9e2461a0c260cc85ebc0bd7e53d3eccde654bcc9
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-07-31 21:28:47 +0900

    RestController, SecurityConfig

--- src/main/java/com/example/geumsabba/SecurityConfig.java
+++ src/main/java/com/example/geumsabba/SecurityConfig.java
@@ -11,9 +11,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
-                .anyRequest().authenticated()
-                .and().csrf().ignoringAntMatchers("/h2-console/**")
-                .and().headers().frameOptions().sameOrigin(); // Enable H2 Console in an iframe
-    }
+                .anyRequest().permitAll() // Allow access to all URLs without authentication
+                .and()
+                .csrf().disable() // Disable CSRF protection
+                .headers().frameOptions().disable(); // Disable X-Frame-Options header, necessary for H2 Console
+      }
 }
--- src/main/java/com/example/geumsabba/controller/MemberController.java
+++ src/main/java/com/example/geumsabba/controller/MemberController.java
@@ -5,11 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MemberController {
 
-//    @Autowired
+    @Autowired
     private MemberRepository memberRepository;
 
     @GetMapping("/geumsabba") //localhost:8080/geumsabba
