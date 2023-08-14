8862608919f678d019668db21227ff42f13981cd
sohyeonJung <jhyeon23@konkuk.ac.kr>
2023-08-14 21:56:38 +0900

    add securitycongif h2 option

--- src/main/java/com/example/geumsabba/SecurityConfig.java
+++ src/main/java/com/example/geumsabba/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.geumsabba;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,8 +13,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().permitAll() // Allow access to all URLs without authentication
+                .antMatchers("/", "/h2-console/**", "/events", "/events/*").permitAll()
                 .and()
                 .csrf().disable() // Disable CSRF protection
                 .headers().frameOptions().disable(); // Disable X-Frame-Options header, necessary for H2 Console
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");  // --> 추가
+    }
+
+
 }
